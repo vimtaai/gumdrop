a0eaf20f4a798c57bd3febdd463d36cffcb3a424
Visnovitz Márton <vimtaai@inf.elte.hu>
2018-05-17 15:56:46 +0200

    Clean up loader code

--- src/load.js
+++ src/load.js
@@ -3,25 +3,29 @@ import parse from './parser';
 const root = document.querySelector('main');
 const cache = {};
 
-export default async function load() {
-  const page = (location.hash === '') ? 'index' : location.hash.substr(1);
-
-  let content;
-
-  if (!cache.hasOwnProperty(page)) {
-    const pageFetchResponse = await fetch('pages/' + page + '.md', {
-      cache: 'no-cache'
-    });
-
-    if (!pageFetchResponse.ok) {
-      const errorFetchResponse =  await fetch('error/' + pageFetchResponse.status + '.md');
-      if (errorFetchResponse.ok) {
-        content = parse(await errorFetchResponse.text());
-      }
-    } else {
-      content = cache[page] = parse(await pageFetchResponse.text());
-    }
+async function fetchPage(page, cache) {
+  if (cache.hasOwnProperty(page)) {
+    return cache[page];
   }
 
-  root.innerHTML = content || cache[page] || '';
+  const response = await fetch('pages/' + page + '.md', { cache: 'no-cache' });
+
+  if (response.ok) {
+    return cache[page] = parse(await response.text());
+  } else {
+    return parse(await fetchErrorPage(response.status));
+  }
 }
+
+async function fetchErrorPage(errorCode) {
+  const response = await fetch('error/' + errorCode + '.md');
+  return response.ok ? await response.text() : '';
+}
+
+async function load() {
+  const page = (location.hash === '') ? 'index' : location.hash.substr(1);
+
+  root.innerHTML = await fetchPage(page, cache);
+}
+
+export default load;
