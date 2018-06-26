500df40dd115a33bdb51d867af45105a66b606c5
Visnovitz Márton <vimtaai@inf.elte.hu>
2018-06-26 15:13:07 +0200

    Add loader (default content of main)

--- src/client/navigate.js
+++ src/client/navigate.js
@@ -2,11 +2,13 @@ import fetchResource from '../network/fetch-resource';
 import transformLoops from './transform-loops';
 
 const root = document.querySelector('main');
+const loader = root.innerHTML;
 
-export async function load() {
+export async function navigate() {
   const page = (location.hash === '') ? 'index' : location.hash.substr(1);
+  await (root.innerHTML = loader);
   root.innerHTML = await fetchResource('pages', page, 'md');
   await transformLoops();
 }
 
-export default load;
+export default navigate;
