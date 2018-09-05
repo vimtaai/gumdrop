b550d3c300805308141d284495260e24ae8c466b
Márton Visnovitz <vimtaai@inf.elte.hu>
2018-09-05 13:56:19 +0200

    Add fragment navigation and nested loops

--- src/client/navigate.js
+++ src/client/navigate.js
@@ -5,12 +5,23 @@ import transformLoops from './transform-loops'
 
 const root = document.querySelector('main')
 const loader = root.innerHTML
+const htmlParser = document.createElement('div')
 
 export async function navigate () {
-  const page = (location.hash === '') ? 'index' : location.hash.substr(1)
+  const [page, fragment] = location.hash.replace(/^#\/?/, '').split('#')
+
   await (root.innerHTML = loader)
-  root.innerHTML = await fetchResource('pages', page, 'md')
-  await transformLoops()
+  htmlParser.innerHTML = await fetchResource('pages', (page || 'index'), 'md')
+  await transformLoops(htmlParser)
+  root.innerHTML = htmlParser.innerHTML
+
+  if (fragment !== undefined) {
+    const element = document.getElementById(fragment)
+
+    if (element !== null) {
+      element.scrollIntoView({ block: 'center', behavior: 'smooth' })
+    }
+  }
 }
 
 export default navigate
--- src/client/transform-loops.js
+++ src/client/transform-loops.js
@@ -2,39 +2,55 @@ import fetchResource from '../network/fetch-resource'
 
 const regex = /\{\{(.+?)\}\}/g
 
-export async function transformLoops () {
-  const loops = Array.from(document.querySelectorAll('[data-each]')).map((node) => ({
-    node,
-    dataFile: node.getAttribute('data-each'),
-    display: node.style.display || null
-  }))
-
-  for (const loop of loops) {
-    loop.node.style.display = 'none'
-    loop.node.removeAttribute('data-each')
+function getValueByPath (obj, path) {
+  if (obj.hasOwnProperty(path)) {
+    return obj[path]
   }
 
-  for (const loop of loops) {
-    const data = await fetchResource('data', loop.dataFile, 'json', [])
-    const parent = loop.node.parentNode
+  const next = path.substring(0, path.indexOf('.'))
 
-    for (const item of data) {
-      const replacer = (_, property) => item[property.trim()]
-      const elem = loop.node.cloneNode(true)
-      const attributes = Array.from(elem.attributes)
+  if (obj.hasOwnProperty(next) && typeof obj[next] === 'object') {
+    return getValueByPath(obj[next], path.substring(path.indexOf('.') + 1))
+  }
 
-      for (const attribute of attributes) {
-        attribute.value = attribute.value.replace(regex, replacer)
-      }
+  return undefined
+}
 
-      elem.innerHTML = elem.innerHTML.replace(regex, replacer)
-      elem.style.display = loop.display
+export async function transformLoops (root, loopData = {}) {
+  const node = root.querySelector('[data-each]')
 
-      parent.insertBefore(elem, loop.node)
+  if (node === null) {
+    return
+  }
+
+  const display = node.style.display || null
+  const parent = node.parentNode
+  const name = node.getAttribute('data-each')
+  const currentData = loopData[name] || await fetchResource('data', name, 'json', [])
+
+  node.style.display = 'none'
+  node.removeAttribute('data-each')
+
+  for (const item of currentData) {
+    const replacer = (_, path) => getValueByPath(item, path)
+    const nodeClone = node.cloneNode(true)
+    const attributes = Array.from(nodeClone.attributes)
+
+    for (const attribute of attributes) {
+      attribute.value = attribute.value.replace(regex, replacer)
     }
 
-    parent.removeChild(loop.node)
+    await transformLoops(nodeClone, item)
+
+    nodeClone.innerHTML = nodeClone.innerHTML.replace(regex, replacer)
+    nodeClone.style.display = display
+
+    parent.insertBefore(nodeClone, node)
   }
+
+  parent.removeChild(node)
+
+  await transformLoops(root)
 }
 
 export default transformLoops
