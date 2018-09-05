37b2f51f0577c023a3f85ef25073f83845eb4c51
Márton Visnovitz <vimtaai@inf.elte.hu>
2018-09-05 16:24:26 +0200

    Change nested loop syntax

--- src/client/transform-loops.js
+++ src/client/transform-loops.js
@@ -2,6 +2,21 @@ import fetchResource from '../network/fetch-resource'
 
 const regex = /\{\{(.+?)\}\}/g
 
+function replacer (data, variableScope) {
+  return function (_, path) {
+    console.log(path, `${variableScope}:`)
+    if (variableScope !== '') {
+      if (!path.startsWith(`${variableScope}:`)) {
+        return `{{${path}}}`
+      }
+
+      return getValueByPath(data, path.split(':')[1])
+    } else {
+      return getValueByPath(data, path)
+    }
+  }
+}
+
 function getValueByPath (obj, path) {
   if (obj.hasOwnProperty(path)) {
     return obj[path]
@@ -16,7 +31,7 @@ function getValueByPath (obj, path) {
   return undefined
 }
 
-export async function transformLoops (root, loopData = {}) {
+export async function transformLoops (root, loopData = {}, loopVariable = '') {
   const node = root.querySelector('[data-each]')
 
   if (node === null) {
@@ -25,24 +40,31 @@ export async function transformLoops (root, loopData = {}) {
 
   const display = node.style.display || null
   const parent = node.parentNode
-  const name = node.getAttribute('data-each')
-  const currentData = loopData[name] || await fetchResource('data', name, 'json', [])
+  const collection = node.getAttribute('data-each')
+
+  let currentData = {}
+  let variableScope = ''
+  if (collection.match(regex)) {
+    variableScope = collection.replace(regex, (_, name) => name)
+    currentData = getValueByPath(loopData, variableScope)
+  } else {
+    currentData = await fetchResource('data', collection, 'json', [])
+  }
 
   node.style.display = 'none'
   node.removeAttribute('data-each')
 
   for (const item of currentData) {
-    const replacer = (_, path) => getValueByPath(item, path)
     const nodeClone = node.cloneNode(true)
     const attributes = Array.from(nodeClone.attributes)
 
     for (const attribute of attributes) {
-      attribute.value = attribute.value.replace(regex, replacer)
+      attribute.value = attribute.value.replace(regex, replacer(item, variableScope))
     }
 
-    await transformLoops(nodeClone, item)
+    await transformLoops(nodeClone, item, variableScope)
 
-    nodeClone.innerHTML = nodeClone.innerHTML.replace(regex, replacer)
+    nodeClone.innerHTML = nodeClone.innerHTML.replace(regex, replacer(item, variableScope))
     nodeClone.style.display = display
 
     parent.insertBefore(nodeClone, node)
