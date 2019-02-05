52f413cb6e775b3f85f187b47107cb3a97820045
Márton Visnovitz <vimtaai@inf.elte.hu>
2019-02-05 20:16:54 +0100

    Allow multiple data files

--- src/network/fetch.js
+++ src/network/fetch.js
@@ -7,43 +7,46 @@ export async function fetchResource(folder, name, type, defaultValue) {
   const response = await window.fetch(`${folder}/${name}.${type}`, { cache: "no-cache" });
 
   if (!response.ok) {
-    return defaultValue || (await fetchContent("errors", response.status, "md", " "));
+    return defaultValue || (await fetchContent("errors", response.status));
   }
 
   return response.text();
 }
 
-export async function fetchData(name, type) {
+export async function fetchData(name, type = "json") {
   if (cache.contains("data", name)) {
     return cache.get("data", name);
   }
 
-  const data = await fetchResource("data", name, type);
+  const data = await fetchResource("data", name, type, {});
 
   return cache.set("data", name, parsers[type](data));
 }
 
-export async function fetchContent(folder, name, type = "") {
+export async function fetchContent(folder, name) {
   if (cache.contains(folder, name)) {
     return cache.get(folder, name);
   }
 
-  const content = await fetchResource(folder, name, type, " ");
+  const content = await fetchResource(folder, name, "md", " ");
 
   const result = frontMatterRegexp.exec(content);
 
   if (result === null) {
-    return cache.set(folder, name, parsers[type](content));
+    return cache.set(folder, name, parsers.md(content));
   }
 
   const [_, front, template] = result;
-  const { data: dataSource, ...context } = parsers.yaml(front);
+  const { data: dataField, ...context } = parsers.yaml(front);
 
-  if (dataSource !== undefined) {
-    context[dataSource] = await fetchData(dataSource, "json");
+  if (dataField !== undefined) {
+    const dataSources = Array.isArray(dataField) ? dataField : [dataField];
+    for (const dataSource of dataSources) {
+      context[dataSource] = await fetchData(dataSource, "json");
+    }
   }
 
   const parsedContent = Mustache.render(template, context);
 
-  return cache.set(folder, name, parsers[type](parsedContent));
+  return cache.set(folder, name, parsers.md(parsedContent));
 }
