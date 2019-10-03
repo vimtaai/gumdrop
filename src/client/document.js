ec537b82eeed8074ebbcfa3e37a80a2600194041
Márton Visnovitz <vimtaai@inf.elte.hu>
2019-10-03 14:32:59 +0200

    Fix bug caused by not having a main tag

--- src/client/document.js
+++ src/client/document.js
@@ -1,3 +1,17 @@
+function getDocumentRoot() {
+  const main = window.document.querySelector("main");
+
+  if (main) {
+    return main;
+  }
+
+  const script = window.document.querySelector("script");
+  const root = window.document.createElement("main");
+  script.parentNode.insertBefore(root, script);
+
+  return root;
+}
+
 export const documentTitle = window.document.title;
-export const documentRoot = window.document.querySelector("main") || window.document.body;
+export const documentRoot = getDocumentRoot();
 export const loaderContent = documentRoot.innerHTML;
